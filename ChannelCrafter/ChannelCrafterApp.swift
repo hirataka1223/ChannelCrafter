a5151e7d767f052c997c6729dd0a3a7ab1edb511
Hiraguchi Takaaki <takaaki.hiraguchi@gmail.com>
2023-11-14 22:49:32 +0900

    Initial Commit

---
+++ ChannelCrafter/ChannelCrafterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelCrafterApp.swift
+//  ChannelCrafter
+//
+//  Created by Taka on 2023/11/14.
+//
+
+import SwiftUI
+
+@main
+struct ChannelCrafterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
